d10ed54e86e7e38755ef282ceb86c6bcd50d8cc0
Jane <akpang.j@northeastern.edu>
2025-09-19 15:27:59 -0400

    Assignment 1

---
+++ Lab1HelloWorld/src/Helloworld/HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Helloworld;
+
+/**
+ *
+ * @author Admin
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+    
+}
